97b832f354d6a0683ac0c75dbdd48f219cd7ed5c
Jean Zapata <jeanza94@hotmail.com>
2022-09-13 13:42:59 -0500

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,38 @@
+import './myLabel.css';
+
+export interface MyLabelProps {
+    /**
+    * this is the content of the span
+    */
+    label: string;
+    /**
+    * this is the size of the span
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+    * do you want all the letter in capital words? 
+    */
+    allCaps?: boolean;
+    /**
+    * is your text primary, secondary or tertiary? 
+    */
+    color?: 'primary' | 'secondary' | 'tertiary'
+    /**
+    * which is the color of your text 
+    */
+    fontColor?: string
+}
+
+export const MyLabel = ({
+    label = 'no label!!',
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor = 'black'
+}: MyLabelProps) => {
+    return (
+        <span className={`label ${size} text-${color}`} style={{ color: fontColor }}>
+            {allCaps ? label.toUpperCase() : label}
+        </span >
+    )
+}
